d4451a9a5d2075c2208606b462a6d1cfbdbb3c38
Vladyslav Chyzhevskyi <coirius@coirius.com>
2013-01-14 17:39:02 +0100

    created window hook

--- StatistiksLib/StatistiksLib.cs
+++ StatistiksLib/StatistiksLib.cs
@@ -1,14 +1,26 @@
-﻿namespace StatistiksLib
+﻿using System;
+using System.Threading;
+namespace StatistiksLib
 {
     public class StatistiksLib
     {
         private KeyboardHook _kHook;
         private MouseHook _mHook;
+        private WindowHook _wHook;
+        private readonly Timer _tTimer;
 
         public StatistiksLib()
         {
             _kHook = new KeyboardHook();
             _mHook = new MouseHook();
+            _wHook = new WindowHook();
+            _tTimer = new Timer(TimerCallback, null, 0, 1000);
+        }
+
+        private void TimerCallback(object state)
+        {
+            var data = _wHook.GetForegroundWindowInfo();
+            Console.WriteLine("Window: {0} {1}", data.Title, data.ExePath);
         }
 
         public void Unhook()
---
+++ StatistiksLib/WindowHook.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Diagnostics;
+using System.Runtime.InteropServices;
+using System.Text;
+
+namespace StatistiksLib
+{
+    internal class Window
+    {
+        public string Title;
+        public string ExePath;
+    }
+
+    internal class WindowHook
+    {
+        internal Window GetForegroundWindowInfo()
+        {
+            IntPtr hWnd = GetForegroundWindow();
+            StringBuilder winCaption = new StringBuilder(GetWindowTextLength(hWnd) + 1);
+            GetWindowText(hWnd, winCaption, winCaption.Capacity);
+            uint pid = 0;
+            GetWindowThreadProcessId(hWnd, out pid);
+            return new Window()
+            {
+                Title = winCaption.ToString(),
+                ExePath = Process.GetProcessById((int)pid).MainModule.FileName
+            };
+        }
+
+        #region Win32 Imports
+        [DllImport("user32.dll")]
+        private static extern IntPtr GetForegroundWindow();
+
+        [DllImport("user32.dll")]
+        private static extern int GetWindowText(IntPtr hWnd, [Out] StringBuilder lpString, int maxCount);
+
+        [DllImport("user32.dll")]
+        private static extern int GetWindowTextLength(IntPtr hWnd);
+
+        [DllImport("user32.dll")]
+        private static extern uint GetWindowThreadProcessId(IntPtr hWnd, out uint lpdwProcessId);
+        #endregion
+    }
+}
