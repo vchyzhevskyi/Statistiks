a3f6beb0138975f985a701cf1610dea6b14fc44a
Vladyslav Chyzhevskyi <coirius@coirius.com>
2013-01-14 16:42:54 +0100

    created library

---
+++ StatistiksLib/Properties/AssemblyInfo.cs
@@ -0,0 +1,18 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("StatistiksLib")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("StatistiksLib")]
+[assembly: AssemblyCopyright("Copyright © Vladyslav Chyzhevskyi, 2013")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("af3db3ef-ce4c-4f1c-8b81-7b8109c90b40")]
+
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
