94c9f0b5b6c6c09387a3dc5fb992b9beff15b40e
Vladyslav Chyzhevskyi <coirius@coirius.com>
2013-01-15 23:05:44 +0100

    added app proj

---
+++ Statistiks.App/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Windows.Forms;
+
+namespace Statistiks.App
+{
+    public class Program : Form
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.Run(new Program());
+        }
+
+        private NotifyIcon _trayIcon;
+        private ContextMenu _trayMenu;
+
+        public Program()
+        {
+            _trayMenu = new ContextMenu();
+            _trayMenu.MenuItems.Add("Get report for current session", GetReportForCurrentSession);
+            _trayMenu.MenuItems.Add("Quit", OnExit);
+
+            _trayIcon = new NotifyIcon();
+            _trayIcon.Text = "Statistiks";
+            _trayIcon.Icon = new Icon(Icon.ExtractAssociatedIcon(Application.ExecutablePath), 64, 64);
+
+            _trayIcon.ContextMenu = _trayMenu;
+            _trayIcon.Visible = true;
+        }
+
+        private void GetReportForCurrentSession(object sender, EventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+
+        private void OnExit(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        protected override void OnLoad(EventArgs e)
+        {
+            Visible = false;
+            ShowInTaskbar = false;
+
+            base.OnLoad(e);
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+                _trayIcon.Dispose();
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ Statistiks.App/Properties/AssemblyInfo.cs
@@ -0,0 +1,18 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Statistiks.App")]
+[assembly: AssemblyDescription("A Statistiks software.")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Statistiks.App")]
+[assembly: AssemblyCopyright("Copyright © Vladyslav Chyzhevskyi, 2013")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("0e2dc03a-d910-4e92-8825-946fb3d61144")]
+
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
